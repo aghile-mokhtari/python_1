0f0bff51f7bb577fb8e3230f75cfde376a4c8159
aghile <aghilemokhtarii@gmail.com>
2023-11-12 22:10:06 +0330

    add guess file

---
+++ guess.py
@@ -0,0 +1,20 @@
+from random import randint
+number = randint(1,40)
+s = 0
+while True:
+    a = int(input('Enter your number: '))
+    if s == 5:
+        print(' we are sorry you lost!')
+        break
+    elif a == number:
+        print('wellDone!!')
+        break
+    elif a > number and a != 0:
+        print(' insert smaller')
+    elif a > 40 or a < 1:
+        print('please insert a number between 1 and 40')
+    else:
+        print(' insert greater')
+    s += 1
+print(number)
+    
